20c0c36ba9bea3318305e918e1aa0e5b5ea55198
limgl1998 <limgl1998@gmail.com>
2020-08-30 20:26:54 +0800

    Level 1. Greet, Echo, Exit
    Implement a skeletal version of Duke that starts by greeting the user, simply echos commands entered by the user, and exits when the user types bye.

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,16 +1,43 @@
+import java.util.Scanner;
+
 public class Duke {
-    public static void main(String[] args) {
+
+    public static void start(){
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
                 + "| | | | | | | |/ / _ \\\n"
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
-        System.out.println("____________________________________________________________");
-        System.out.println(" Hello! I'm Duke");
-        System.out.println(" What can I do for you?");
-        System.out.println("____________________________________________________________");
-        System.out.println(" Bye. Hope to see you again soon!");
-        System.out.println("____________________________________________________________");
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Hello! I'm Duke");
+        System.out.println("    What can I do for you?");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void exit(){
+        System.out.println("    ____________________________________________________________");
+        System.out.println("    Bye. Hope to see you again soon!");
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void handlesCommand(String command){
+        System.out.println("    ____________________________________________________________");
+        System.out.println("     " + command);
+        System.out.println("    ____________________________________________________________");
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        start();
+        String command = in.nextLine();
+        while (command.equals("bye")==false) {
+            handlesCommand(command);
+            command = in.nextLine();
+        }
+
+        exit();
+
     }
 }
